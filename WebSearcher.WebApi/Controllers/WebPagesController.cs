50412eb0e17d4b43e27cdd34c8c20172e4267144
Adam Wójcik <adamwojcik.programista@gmail.com>
2019-10-16 19:24:53 +0200

    Created working web api

---
+++ WebSearcher.WebApi/Controllers/WebPagesController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WebSearcher.DataAccess.Abstract;
+using WebSearcher.Entities;
+
+namespace WebSearcher.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WebPagesController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public WebPagesController(IUnitOfWorkFactory unitOfWorkFactory)
+        {
+            _unitOfWork = unitOfWorkFactory.Create();
+        }
+
+        [HttpGet]
+        public IEnumerable<WebPage> Get()
+        {
+            return _unitOfWork.WebPages.GetAll();
+        }
+    }
+}
